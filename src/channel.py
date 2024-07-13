47217ab8c66aed73f337e5cfbbc6dc892744e8c8
Svetlana <36902132+svkote@users.noreply.github.com>
2024-07-13 23:12:41 +0300

    класс Channel в файле src/channel.py и  метод print_info() класса Channel

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+import os
+import json
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Channel:
@@ -5,8 +9,20 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key = os.getenv("YOUTUBE_API_KEY_FOR_SKYPRO")
+        if not self.api_key:
+            raise ValueError("Не удалось найти API ключ. Проверьте переменную окружения YOUTUBE_API_KEY_FOR_SKYPRO.")
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        try:
+            request = self.youtube.channels().list(
+                part='snippet,statistics',
+                id=self.channel_id
+            )
+            response = request.execute()
+            print(json.dumps(response, indent=2, ensure_ascii=False))
+        except HttpError as e:
+            print(f"An HTTP error occurred: {e}")
